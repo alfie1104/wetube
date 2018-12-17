f245c6bdff7e68d221396e30b148d267ef4f709c
peter_alfie <alfie1104@gmail.com>
2018-12-17 15:16:43 +0900

    Local variables are added

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ import morgan from "morgan"; //logger middleware
 import helmet from "helmet"; //security middleware
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares"; //로컬 변수를 view페이지에서 사용하기 위해 localMiddleware를 생성하였음. res.locals.XXX형식으로 변수 생성 후 view 페이지에서 #{XXX}형식으로 변수 접근가능
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev")); //logging configuration
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,10 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
+export const home = (req, res) => res.render("home", {pageTitle : "Home"}); //render의 두번째 인자로 전달된 값은 view page에서 #{key}로 획득가능
+export const search = (req, res) => {
+    const {query: {term : searchingBy}} = req; //const searchingBy = req.query.term 과 같음
 
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+    res.render("search", {pageTitle : "Search", searchingBy});  //뒤에 searchingBy는 키와 값의 이름이 같으므로 searchingBy : searchingBy에서 searchingBy로 생략가능(ES6)
+};
+export const upload = (req, res) => res.render("upload", {pageTitle:"Upload"});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"Video Detail"});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"Edit Video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle:"Delete Video"});
