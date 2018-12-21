2e7cf6d7aabd5899817237fb05d5f7f5cbae65f4
peter_alfie <alfie1104@gmail.com>
2018-12-21 16:00:04 +0900

    update scss files

--- middlewares.js
+++ middlewares.js
@@ -1,14 +1,14 @@
 import multer from "multer";
 import routes from "./routes";
 
-const multerVideo = multer({dest:"uploads/videos/"});
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube"; //view 페이지들에서 사용할 local 변수들을 res.locals에 생성. view 페이지에서는 #{변수명}으로 local변수 획득가능
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated:true,
-        id:1
+        isAuthenticated: false,
+        id: 1
     }
     next(); //미들웨어이므로 다음 라우터로 넘어갈 수 있도록 next를 호출해야함.
 };
