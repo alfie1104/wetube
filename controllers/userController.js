7c66aaf7f0387016b0996bdba33b0250c5e1bc9b
peter_alfie <alfie1104@gmail.com>
2018-12-28 08:35:44 +0900

    passport-github is installed

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,8 +35,14 @@ export const postLogin = passport.authenticate('local', {
     successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+    console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const githubLogin = passport
+
 export const logout = (req, res) => {
-    //To Do : Process Log Out
+    req.logout();
     res.redirect(routes.home);
 };
 
--- passport.js
+++ passport.js
@@ -1,7 +1,18 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";
+import dotenv from "dotenv";
 import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userController";
+
+dotenv.config(); //.env 파일에 적힌 내용을 process.env에 등록시켜줌
 
 passport.use(User.createStrategy());
+passport.use(new GithubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SEC,
+    callbackURL: "http://localhost:4000/auth/github/callback"
+}, githubLoginCallback)
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
