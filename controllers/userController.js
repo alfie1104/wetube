970a0c069a4a746603d418af898762e39fad589f
peter_alfie <alfie1104@gmail.com>
2019-01-02 14:25:41 +0900

    user detail is updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,7 +39,7 @@ export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
 
-    const { _json: { id, avatar_url, name, email, displayName } } = profile;
+    const { _json: { id, avatar_url: avatarUrl, name, email, displayName } } = profile;
 
     console.log(email);
     console.log(displayName);
@@ -58,7 +58,7 @@ export const githubLoginCallback = async (_, __, profile, cb) => {
             email: displayName,
             name,
             githubId: id,
-            avatarUrl: avatar_url
+            avatarUrl
         });
         return cb(null, newUser);
     } catch (error) {
@@ -75,6 +75,10 @@ export const postGithubLogIn = (req, res) => {
 export const logout = (req, res) => {
     req.logout();
     res.redirect(routes.home);
+};
+
+export const getMe = (req, res) => {
+    res.render("userDetail", { pageTitle: "User Detail", user: req.user });
 };
 
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
--- middlewares.js
+++ middlewares.js
@@ -6,7 +6,7 @@ const multerVideo = multer({ dest: "uploads/videos/" });
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube"; //view 페이지들에서 사용할 local 변수들을 res.locals에 생성. view 페이지에서는 #{변수명}으로 local변수 획득가능
     res.locals.routes = routes;
-    res.locals.user = req.user || null;
+    res.locals.loggedUser = req.user || null;
     next(); //미들웨어이므로 다음 라우터로 넘어갈 수 있도록 next를 호출해야함.
 };
 
